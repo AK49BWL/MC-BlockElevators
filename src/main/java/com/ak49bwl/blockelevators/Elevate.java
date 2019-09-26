959cda9bd17f3a02efc07a9e34429b1d100e3f93
Jeremy Luchak <jeremy@nevada.unr.edu>
2019-09-26 11:28:28 -0700

    refine the cannibalized code, comments added, variables renamed for readability

--- src/main/java/com/ak49bwl/blockelevators/Elevate.java
+++ src/main/java/com/ak49bwl/blockelevators/Elevate.java
@@ -4,7 +4,6 @@ import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerMoveEvent;
@@ -12,47 +11,50 @@ import org.bukkit.event.player.PlayerToggleSneakEvent;
 
 public class Elevate implements Listener {
 
-    // Changeable Vars
-    int maxElev = 64; // Max elevation change
-    Material block = Material.IRON_BLOCK; // Elevator Block
+    // Configurable Vars
+    private final int MAX_ELEV = 64; // Max elevation change
+    private final Material TRIGGER_TYPE = Material.IRON_BLOCK; // Elevator Block
 
+    //Constants named for readability
+    private final int ABOVE = 1;
+    private final int BELOW = -1;
 
     @EventHandler
-    public void ElevDown(PlayerToggleSneakEvent e) {
-        Player p = e.getPlayer();
-        Block b = p.getLocation().getBlock().getRelative(BlockFace.DOWN);
+    public void ElevDown(PlayerToggleSneakEvent event) {
+        if(! event.isSneaking() ) return; //person is coming out of sneak, ignore
+        //this will still execute if you step on a slab, stair, piston push up, explosion, etc
 
-        if (p.isSneaking() && b.getType().equals(block)) {
-            b = b.getRelative(BlockFace.DOWN, 3);
-            int i = maxElev;
-            while (i > 0 && !(b.getType() == block && b.getRelative(BlockFace.UP).getType().equals(Material.AIR) && b.getRelative(BlockFace.UP, 2).getType().equals(Material.AIR)) ) {
-                i--;
-                b = b.getRelative(BlockFace.DOWN);
-            }
-            if (i > 0) {
-                Location l = p.getLocation();
-                l.setY(l.getY() - maxElev - 3 + i);
-                p.teleport(l);
-            }
-        }
+        Block underneath = event.getPlayer().getLocation().getBlock().getRelative(BlockFace.DOWN);
+        if(! underneath.getType().equals(TRIGGER_TYPE) )return; //not directly above a trigger, ignore
+
+        Location destination = getDestination( event.getPlayer().getLocation(), BELOW );
+        if( destination == null ) return; //no valid spot found, ignore
+
+        event.getPlayer().teleport(destination);
     }
-
     @EventHandler
-    public void ElevUp(PlayerMoveEvent e) {
-        Player p = e.getPlayer();
-        Block b = e.getTo().getBlock().getRelative(BlockFace.DOWN);
-        if (e.getFrom().getY() < e.getTo().getY() && b.getType().equals(block)) {
-            b = b.getRelative(BlockFace.UP, 3);
-            int i = maxElev;
-            while (i > 0 && !(b.getType() == block && b.getRelative(BlockFace.UP).getType().equals(Material.AIR) && b.getRelative(BlockFace.UP, 2).getType().equals(Material.AIR)) ) {
-                i--;
-                b = b.getRelative(BlockFace.UP);
-            }
-            if (i > 0) {
-                Location l = p.getLocation();
-                l.setY(l.getY() + maxElev + 3 - i);
-                p.teleport(l);
+    public void ElevUp(PlayerMoveEvent event) {
+        if( event.getFrom().getY() >= event.getTo().getY() ) return; //ignore falling and horizontal
+        //this will still execute if you step on a slab, stair, piston push up, explosion, etc
+
+        Block underneath = event.getPlayer().getLocation().getBlock().getRelative(BlockFace.DOWN);
+        if(! underneath.getType().equals(TRIGGER_TYPE) )return; //not directly above a trigger, ignore
+
+        Location destination = getDestination( event.getPlayer().getLocation(), ABOVE );
+        if( destination == null ) return; //no valid spot found, ignore
+
+        event.getPlayer().teleport(destination);
+    }
+    private Location getDestination( Location from , int directionChanger ){
+        Location checkMe;
+        for( int searchY = 0; searchY < MAX_ELEV; searchY++ ){
+            checkMe = from.add( 0, searchY*directionChanger, 0 );
+            if( checkMe.getBlock().getType().equals(TRIGGER_TYPE) &&
+                checkMe.getBlock().getRelative(BlockFace.UP, 1).getType().equals(Material.AIR) &&
+                checkMe.getBlock().getRelative(BlockFace.UP, 2).getType().equals(Material.AIR) ){
+                return checkMe.add(0,1,0); //checkMe is the Trigger, return the spot immediately above it
             }
         }
+        return null;
     }
 }
