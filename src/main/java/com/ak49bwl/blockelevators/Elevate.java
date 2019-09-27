49099d015d3dc23d6352f056d3df44bb508909d6
AK49BWL <bwl1989@gmail.com>
2019-09-26 20:05:57 -0500

    Version change, debugging (works)

--- src/main/java/com/ak49bwl/blockelevators/Elevate.java
+++ src/main/java/com/ak49bwl/blockelevators/Elevate.java
@@ -1,5 +1,6 @@
 package com.ak49bwl.blockelevators;
 
+import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
@@ -47,13 +48,14 @@ public class Elevate implements Listener {
     }
     private Location getDestination( Location from , int directionChanger ){
         Location checkMe;
-        for( int searchY = 0; searchY < MAX_ELEV; searchY++ ){
+        for( int searchY = 2; searchY < MAX_ELEV; searchY++ ){ // searchY can't be <2 because it will see the block we're standing on.
             checkMe = from.add( 0, searchY*directionChanger, 0 );
+            Bukkit.getLogger().info("Current player location: " + from.getY() + ", Looking at block " + searchY*directionChanger + ", " + checkMe.getY());
             if( checkMe.getBlock().getType().equals(TRIGGER_TYPE) &&
                 checkMe.getBlock().getRelative(BlockFace.UP, 1).getType().equals(Material.AIR) &&
                 checkMe.getBlock().getRelative(BlockFace.UP, 2).getType().equals(Material.AIR) ){
                 return checkMe.add(0,1,0); //checkMe is the Trigger, return the spot immediately above it
-            }
+            } else from.subtract( 0, searchY*directionChanger, 0 );
         }
         return null;
     }
